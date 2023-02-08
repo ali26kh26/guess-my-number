606b280dcbf4b4777e22c1b739fca6600388dd12
ali26kh26 <fcb.akh.26@gmail.com>
2023-02-08 20:53:22 +0330

    ADD: add background to app

--- guess-my-number/App.js
+++ guess-my-number/App.js
@@ -1,8 +1,22 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import StartGameScreen from "./pages/start-game-screen";
-
+import { LinearGradient } from "expo-linear-gradient";
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={["#33031b", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        style={styles.rootScreen}
+        imageStyle={{ opacity: 0.2 }}
+        source={require("./assets/images/background.png")}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
