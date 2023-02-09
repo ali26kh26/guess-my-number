c25bb528797d487bad788cd57bd7ed7acc64405c
ali26kh26 <fcb.akh.26@gmail.com>
2023-02-09 21:35:29 +0330

    ADD: create game screen and game logic

--- guess-my-number/pages/game-screen.js
+++ guess-my-number/pages/game-screen.js
@@ -1,11 +1,66 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import GuessNumber from "../components/UI/game/guess-number";
+import PrimaryButton from "../components/UI/primary-button";
+import Title from "../components/UI/title";
 
-const GameScreen = () => {
+let minNumber = 1;
+let maxNumber = 100;
+
+const GameScreen = ({ userNumber }) => {
+  const [phoneGuess, setphoneGuess] = useState(
+    generateRandomBetween(minNumber, maxNumber, userNumber)
+  );
+
+  function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
+  const nextGuessGenerator = (direction) => {
+    if (
+      (direction === "lower" && phoneGuess < userNumber) ||
+      (direction === "greater" && phoneGuess > userNumber)
+    ) {
+      Alert.alert("Dont Lie!!", "you know that this is wrong...", [
+        { text: "Sorry", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      maxNumber = phoneGuess;
+    } else if (direction === "greater") {
+      minNumber = phoneGuess;
+    }
+    setphoneGuess(generateRandomBetween(minNumber, maxNumber, phoneGuess));
+  };
+
   return (
-    <View>
-      <Text>Game Screen!!!</Text>
+    <View style={styles.container}>
+      <Title>Opponent Guess</Title>
+      <GuessNumber>{phoneGuess}</GuessNumber>
+      <View>
+        <Text>lower or higher ?</Text>
+        <PrimaryButton onPress={() => nextGuessGenerator("lower")}>
+          -
+        </PrimaryButton>
+        <PrimaryButton onPress={() => nextGuessGenerator("greater")}>
+          +
+        </PrimaryButton>
+      </View>
     </View>
   );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+    marginTop: 16,
+  },
+});
 export default GameScreen;
