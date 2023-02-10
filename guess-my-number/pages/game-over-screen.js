8e287c5d7bba00bed5b5d06998882f1c30d3fd4e
ali26kh26 <fcb.akh.26@gmail.com>
2023-02-10 19:51:29 +0330

    ADD: make game over screen look better

--- guess-my-number/pages/game-over-screen.js
+++ guess-my-number/pages/game-over-screen.js
@@ -1,11 +1,41 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Title from "../components/UI/title";
+import colors from "../utils/colors";
 
 const GameOverScreen = () => {
   return (
-    <View>
-      <Text>Game is over</Text>
+    <View style={styles.rootContainer}>
+      <View style={{ width: "100%" }}>
+        <Title>Game Over</Title>
+      </View>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    padding: 16,
+    alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    overflow: "hidden",
+    borderWidth: 3,
+    borderColor: colors.primary900,
+    margin: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default GameOverScreen;
